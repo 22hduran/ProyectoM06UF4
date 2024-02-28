3012ca9504e7b92e835f243d37445f3f6c2b3802
Raul Sandez <raaulee@gmail.com>
2024-02-28 16:55:38 +0100

    update

--- app/index.js
+++ app/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
 
+const { Client } = require('pg');
+
+const connectionData = {
+
+  user: 'user',
+
+  host: '',
+
+  database: '',
+
+  password: 'mysecretpassword',
+
+  port: 5432,
+
+}
+
+const client = new Client(connectionData)
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
