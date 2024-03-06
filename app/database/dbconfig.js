6b8ee7eec6c92bd04cb65902286347d766b31ddd
Iugaa0 <dani04iuga@gmail.com>
2024-03-06 15:58:48 +0100

    refactor

--- app/database/dbconfig.js
+++ app/database/dbconfig.js
@@ -3,11 +3,7 @@ const data_connection = {
     port: 5432,
     database: 'Sakila',
     user: 'postgres',
-<<<<<<< HEAD
     password: '6619csh2018',
-=======
-    password: '8865',
->>>>>>> a6ce4ea5d986ed36aeadfa09e2e030ea81813d3f
 }
 
 module.exports = data_connection;
