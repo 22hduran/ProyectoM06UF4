3747ed737dfcdac07181235232906807ee22829f
Iugaa0 <dani04iuga@gmail.com>
2024-03-12 17:06:59 +0100

    add put function

--- app/controllers/films.js
+++ app/controllers/films.js
@@ -27,14 +27,12 @@ router.get('/', async (req, res) => {
 
 router.post('/crear', async (req, res) => {
     const { title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost } = req.body;
-    console.log(req.body);
 
     const client = new Client(db);
     try {
         await client.connect();
         const result = await client.query('INSERT INTO film (title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *', 
             [title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost]);
-        console.log('new film: ' + result.json());
 
         res.status(201).json(result.rows[0]);
     } catch (error) {
@@ -45,18 +43,43 @@ router.post('/crear', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const client = new Client(db);
+    try {
+        await client.connect();
+        const result = await client.query('SELECT * FROM film WHERE film_id = $1', [id]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'Película no encontrada' });
+        }
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    } finally {
+        client.end();
+    }
+});
+
+
 router.put('/:id', async (req, res) => {
     const { id } = req.params;
     const { title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost } = req.body;
+    console.log('body: ');
+    console.log(req.body);
+
+    if (!title || !description || !release_year || !language_id || !rental_duration || !rental_rate || !length || !replacement_cost) {
+        return res.status(400).json({ message: 'Todos los campos son requeridos' });
+    }
 
     const client = new Client(db);
     try {
         await client.connect();
 
-        const existingFilm = await client.query('SELECT * FROM film WHERE film_id = $1', [id]);
-        if (existingFilm.rows.length === 0) {
-            return res.status(404).json({ message: 'Película no encontrada' });
-        }
+        // const existingFilm = await client.query('SELECT * FROM film WHERE film_id = $1', [id]);
+        // if (existingFilm.rows.length === 0) {
+        //     return res.status(404).json({ message: 'Película no encontrada' });
+        // }
 
         const result = await client.query(
             'UPDATE film SET title = $1, description = $2, release_year = $3, language_id = $4, rental_duration = $5, rental_rate = $6, length = $7, replacement_cost = $8 WHERE film_id = $9 RETURNING *',
@@ -71,7 +94,6 @@ router.put('/:id', async (req, res) => {
         client.end();
     }
 });
-
 
 router.delete('/:id', async (req, res) => {
     const { id } = req.params;
